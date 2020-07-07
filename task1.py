0b46ca5b2285052fd4c3e4f7885f127da7ba8f2e
Igor Flyunt <igorflyunt2@gmail.com>
2020-07-07 20:32:38 +0300

    add task 1 and task 2

---
+++ task1.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+print('a + b =', a + b)
+print('a - b =', a - b)
+print('a * b =', a * b)
+print('a / b =', a / b)
+print('a // b =', a // b)
+print('a % b =', a % b)
+print('a ** b =', a ** b)
+print('type = a', type(a) , 'type = b', type(b) )
---
+++ task2.py
@@ -0,0 +1,4 @@
+name = input('What is your name?')
+old = input('How old are you?')
+live = input('Where do you live?')
+print('Hello,',  name ,  'Your age is' , old , 'You live in' , live , sep=' ')
